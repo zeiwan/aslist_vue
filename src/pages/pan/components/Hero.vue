72fc64a2735cd17d251033532e4b4c340f81193d
mogenle <mogenle@163.com>
2024-10-18 18:45:49 +0800

    1

---
+++ src/pages/pan/components/Hero.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+const router = useRouter();
+
+function testStore() {
+  router.push({
+    name: "StoreTest",
+  });
+}
+</script>
+
+<template>
+<!--  <div class="mt-[-4rem] min-h-screen text-primary-content hero"> -->
+<!--    &lt;!&ndash;  <div class="hero min-h-screen from-primary to-secondary" style="background-image: url(https://img.js.design/assets/Resources/background/home-bg-2.png);"> &ndash;&gt; -->
+<!--    <div -->
+<!--      class="place-items-center from-primary to-base-100 bg-gradient-to-bl hero-overlay" -->
+<!--    /> -->
+<!--    <div class="text-center hero-content"> -->
+<!--      <div class="max-w-md"> -->
+<!--        <h1 class="mb-5 text-5xl font-bold"> -->
+<!--          Boot Vue -->
+<!--        </h1> -->
+<!--        <p class="mb-5"> -->
+<!--          Mocking up web app with Boot Vue <sup>(speed)</sup> -->
+<!--        </p> -->
+<!--        <button class="btn btn-primary" @click="testStore"> -->
+<!--          Test Store -->
+<!--        </button> -->
+<!--      </div> -->
+<!--    </div> -->
+<!--  </div> -->
+</template>
+
+<style scoped></style>
---
+++ src/pages/pan/store/index.vue
@@ -0,0 +1,122 @@
+<script lang="ts" setup>
+import { computed, onMounted, ref, shallowRef } from "vue";
+import { breakpointsTailwind, useBreakpoints } from "@vueuse/core";
+import { useRequest } from "alova/client";
+import Table from "./Table.vue";
+import Grid from "~/pages/pan/store/Grid.vue";
+import EditFile from "~/pages/pan/store/modules/editFile.vue";
+import { getFileList } from "~/api";
+
+// 引用组件函数
+const editRef = shallowRef<InstanceType<typeof EditFile>>();
+const breakpoints = useBreakpoints(breakpointsTailwind);
+const isMobile = breakpoints.smaller("sm");
+const rootPath = ref([""]);
+const commonPath = ref("/");
+const type = ref(1);
+
+// 切换视图
+const currentComponent = computed(() => type.value === 1 ? Table : Grid);
+
+const { data, loading, send, error } = useRequest(() => getFileList({ path: commonPath.value }));
+
+function handleError(error: any) {
+  console.error("请求失败:", error);
+  ElMessage.error("请求失败，请稍后再试！");
+}
+
+async function getFiles(common = "/") {
+  if (common === "") {
+    commonPath.value = "/";
+  } else {
+    commonPath.value = common;
+  }
+  try {
+    send();
+  } catch (err) {
+    handleError(err);
+  }
+}
+onMounted(() => {
+  // getFiles();
+});
+
+// 设置路径 来自子组件点击事件
+function setPath(path: string) {
+  rootPath.value.push(path);
+  commonPath.value = rootPath.value.join("/");
+  getFiles(rootPath.value.join("/"));
+}
+
+// 点击面包屑
+function goMenu(item: string) {
+  const index = rootPath.value.indexOf(item);
+  rootPath.value = rootPath.value.slice(0, index + 1);
+  getFiles(rootPath.value.join("/"));
+}
+
+// 返回上层
+function goBack() {
+  rootPath.value.pop();
+  getFiles(rootPath.value.join("/"));
+}
+
+// 视图切换
+function cut() {
+  type.value = 2;
+}
+
+function upload() {
+  ElMessage.info("暂未开发完成！~");
+}
+
+// 新建文件
+function createFile() {
+  editRef.value?.open("add");
+  console.log("打开新建文件夹");
+}
+
+function close() {
+  // showEdit.value = false;
+}
+</script>
+
+<template>
+  <div>
+    <ElCard>
+      <div class="flex pb space-x-4">
+        <div class="text-white btn btn-info btn-sm" @click="upload">
+          上传文件
+        </div>
+        <div class="text-white btn btn-success btn-sm" @click="createFile">
+          新建文件
+        </div>
+        <div class="text-white btn btn-error btn-sm">
+          批量删除
+        </div>
+      </div>
+      <ElCard class="el-card__header" shadow="never">
+        <template #header>
+          <div class="text-sm breadcrumbs">
+            <ul>
+              <li v-show="rootPath.length > 1">
+                <a @click="goBack">返回</a>
+              </li>
+              <li v-for="item in rootPath" :key="item">
+                <a @click="goMenu(item)">{{ item || '全部' }}</a>
+              </li>
+            </ul>
+          </div>
+        </template>
+        <component :is="currentComponent" :is-loading="loading" :is-mobile="isMobile" :table-data="data?.records" @send-path="setPath" />
+      </ElCard>
+    </ElCard>
+  </div>
+  <EditFile ref="editRef" title-type @close="close" />
+</template>
+
+<style scoped>
+.el-card__header {
+  --el-card-padding: 5px;
+}
+</style>
